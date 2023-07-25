a57a07a912cd1a45a455b1ff7b9ee96d11417b64
Pawan Kumar <pawanbtechcse752000@gmail.com>
2023-07-25 23:55:37 +0530

    context api

--- usecontext-hook/src/components/CompA.js
+++ usecontext-hook/src/components/CompA.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { context } from './context/AppContext';
 import CompB from "./CompB"
 
 function CompA(){
+    const {quality,name} = useContext(context);
     return(
         <div>
-        <h2>Component A</h2>
+        <h2>Component A {quality} and {name}</h2>
         <CompB />
         </div>
     )
--- usecontext-hook/src/components/CompB.js
+++ usecontext-hook/src/components/CompB.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import CompC from "./CompC"
+import {context} from "./context/AppContext"
+                   
 function CompB(){
+    const {quality,name} = useContext(context)
     return(
         <div>
-       <h2>Component B</h2>
+       <h2>Component B {quality} and {name}</h2>
        <CompC />
         </div>
     )
